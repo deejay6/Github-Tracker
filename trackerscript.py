0e4f92dd219bf764745e22a8a7238db8546f18c2
Rohan Khurana <rohan.kumquat@gmail.com>
2017-06-27 18:15:45 +0530

    CSV based Git tracker

--- trackerscript.py
+++ trackerscript.py
@@ -1,26 +1,35 @@
-import requests, json, csv
-import pandas
+import requests
+import json
+import csv
 
 BASE_URL = "https://api.github.com"
 
-username = ['rk2810', 'deejay06', 'princeyadav05']
-repo = ['gemini', 'SpyChat-Application', 'spy_chat']
+with open('username.csv', 'rb') as f:
+    reader = csv.reader(f)
+    username = list(reader)
+print username
 
+with open('repo.csv', 'rb') as f:
+    reader = csv.reader(f)
+    repo = list(reader)
+print repo
 
-for i in range(0, len(username)):
-    request_url = (BASE_URL + '/repos/%s/%s/stats/contributors') % (username[i], repo[i])
+z = len(username[0])
+x = username[0]
+y = repo[0]
+print z
+
+for i in range(0, z):
+    request_url = (BASE_URL + '/repos/%s/%s/stats/contributors') % (x[i], y[i])
     print 'GET request url : %s' % request_url
     data = requests.get(request_url).json()
     with open('repo_data.json', 'w') as outfile:     # create a json data dump
         json.dump(data, outfile)
 
-# load the json data dump to a csv file
-
 f = open('repo_data.json')
 data = json.load(f)
 f.close()
-
-f=csv.writer(open('output.csv','wb+'))
+f = csv.writer(open('output.csv','wb+'))
 
 for item in data:
     f.writerow([item['weeks'], item['total']] + item['author'].values())
