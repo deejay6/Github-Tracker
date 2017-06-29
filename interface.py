fb5883b67ff0af5972f4816ea1a84c7cc74e8f31
Rohan Khurana <rohan.kumquat@gmail.com>
2017-06-29 10:47:30 +0530

    unused file removal

--- interface.py
+++
@@ -1,9 +0,0 @@
-def copy_csv():
-    with open("output.csv") as f2:
-        lines = f2.readlines()
-        lines = [l for l in lines if "ROW" in l]
-        with open("comparator.csv", "w") as f1:
-            f1.writelines(lines)
-
-
-copy_csv()
